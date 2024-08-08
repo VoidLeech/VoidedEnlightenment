1cdac4934e2a30ec72aa25c1af44e3d893ca9ff0
VoidLeech <ian.gerritse@gmail.com>
2024-08-08 23:48:42 +0200

    chore: re-implement placing of stalk0 (todo: shrink stack)

--- src/main/java/com/github/voidleech/voided_enlightenment/mixin/cerulean_stalk/PlantStalkMixin.java
+++ src/main/java/com/github/voidleech/voided_enlightenment/mixin/cerulean_stalk/PlantStalkMixin.java
@@ -1,8 +1,16 @@
 package com.github.voidleech.voided_enlightenment.mixin.cerulean_stalk;
 
 import net.mcreator.enlightened_end.procedures.PlantCeruleanStalkProcedure;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlantCeruleanStalkProcedure.class)
 public class PlantStalkMixin {
+    @Inject(method = "onRightClickBlock", at = @At("HEAD"), remap = false, cancellable = true)
+    private static void ve$cancelPlacement(PlayerInteractEvent.RightClickBlock event, CallbackInfo ci){
+        ci.cancel();
+    }
 }
--- src/main/java/com/github/voidleech/voided_enlightenment/mixin/cerulean_stalk/StalkItemMixin.java
+++ src/main/java/com/github/voidleech/voided_enlightenment/mixin/cerulean_stalk/StalkItemMixin.java
@@ -1,8 +1,32 @@
 package com.github.voidleech.voided_enlightenment.mixin.cerulean_stalk;
 
+import net.mcreator.enlightened_end.init.EnlightenedEndModBlocks;
 import net.mcreator.enlightened_end.item.CeruleanStalkItem;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(CeruleanStalkItem.class)
-public class StalkItemMixin {
+public class StalkItemMixin extends Item {
+    public StalkItemMixin(Properties pProperties) {
+        super(pProperties);
+    }
+
+    @Override
+    public InteractionResult useOn(UseOnContext pContext) {
+        BlockPlaceContext ctx = new BlockPlaceContext(pContext);
+        BlockState stalk0 = EnlightenedEndModBlocks.CERULEAN_STALK_0.get().getStateForPlacement(ctx);
+        Level level = ctx.getLevel();
+        if (ctx.canPlace() && stalk0.canSurvive(level, ctx.getClickedPos())){
+            level.setBlock(ctx.getClickedPos(), stalk0, Block.UPDATE_ALL);
+            return InteractionResult.sidedSuccess(level.isClientSide());
+        }
+        return super.useOn(pContext);
+    }
 }
