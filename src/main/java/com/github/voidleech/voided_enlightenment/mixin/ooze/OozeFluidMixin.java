1ec336e59d1ed5d453c6a804c57af15a7ffd331c
VoidLeech <ian.gerritse@gmail.com>
2024-08-11 16:57:23 +0200

    feat: ooze fluid less water-like

---
+++ src/main/java/com/github/voidleech/voided_enlightenment/mixin/ooze/OozeFluidMixin.java
@@ -0,0 +1,22 @@
+package com.github.voidleech.voided_enlightenment.mixin.ooze;
+
+import net.mcreator.enlightened_end.fluid.types.OozeFluidFluidType;
+import net.minecraftforge.fluids.FluidType;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(OozeFluidFluidType.class)
+public class OozeFluidMixin extends FluidType {
+    public OozeFluidMixin(Properties properties) {
+        super(properties);
+    }
+
+    @Inject(method = "<init>", at = @At("TAIL"))
+    private void ve$newOozeProperties(CallbackInfo ci){
+        this.canConvertToSource = false;
+        this.canSwim = false;
+        this.viscosity = 2000; // Double the viscosity of water
+    }
+}
