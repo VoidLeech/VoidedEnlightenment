9cdaecec70e2910136aa86ca5fdc9480dda7c6fb
VoidLeech <ian.gerritse@gmail.com>
2024-09-04 15:54:09 +0200

    chore: update to oblivion 1.1.0

--- src/main/java/com/github/voidleech/voided_enlightenment/registry/VEPacks.java
+++ src/main/java/com/github/voidleech/voided_enlightenment/registry/VEPacks.java
@@ -1,13 +1,13 @@
 package com.github.voidleech.voided_enlightenment.registry;
 
-import com.github.voidleech.oblivion.registry.AbstractObsidianPacks;
+import com.github.voidleech.oblivion.registry.AbstractOblivionPacks;
 import com.github.voidleech.voided_enlightenment.VoidedEnlightenment;
 import net.minecraft.network.chat.Component;
 import net.minecraft.util.Tuple;
 
 import java.util.Map;
 
-public class VEPacks extends AbstractObsidianPacks {
+public class VEPacks extends AbstractOblivionPacks {
     private static final Map<String, Tuple<Component, Boolean>> DESCRIPTIONS = Map.of(
             "voided_enlightenment", new Tuple<>(Component.literal("Enlightend asset overrides"), true));
 
